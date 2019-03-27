ba69d3306ca6e3691129811a253197ecf93ad921
Andy Aldo Dharmawan <andyaldodharmawan@gmail.com>
2019-03-27 21:02:20 +0800

    Add nvm and purity-prompt installation

--- install.sh
+++ install.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 
 sh brew/install.sh
+sh nvm/install.sh
+
+npm install -g purity-prompt
+
 sh zsh/install.sh
---
+++ nvm/install.sh
@@ -0,0 +1,3 @@
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh | bash
+
+nvm install node
