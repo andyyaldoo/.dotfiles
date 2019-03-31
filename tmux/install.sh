d77c428b60f429989e5c782e8f7c24085e9ef957
Andy Aldo Dharmawan <andyaldodharmawan@gmail.com>
2019-03-31 19:14:33 +0800

    Clone tmux package manager

--- tmux/install.sh
+++ tmux/install.sh
@@ -1,3 +1,7 @@
 # Install tmux
 
+mkdir -p $HOME/.tmux/plugins/tpmm
+
+git clone https://github.com/tmux-plugins/tpm $HOME/.tmux/plugins/tpm
+
 ln -s $HOME/.dotfiles/tmux/.tmux.conf $HOME/.tmux.conf
