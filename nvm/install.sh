1a16e40dacafbd9493ced4b3356aa517f63297c7
Andy Aldo Dharmawan <andyaldodharmawan@gmail.com>
2019-03-31 19:18:23 +0800

    Fix nvm install script

--- nvm/install.sh
+++ nvm/install.sh
@@ -1,3 +1,4 @@
+NVM_DIR=$HOME/.nvm
 mkdir -p $NVM_DIR
 [ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh"  # This loads nvm
 [ -s "/usr/local/opt/nvm/etc/bash_completion" ] && . "/usr/local/opt/nvm/etc/bash_completion"  # This loads nvm bash_completion
