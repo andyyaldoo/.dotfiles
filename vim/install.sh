020c127a2c0c80567a0f0950ebb2a75820a2f05a
Andy Aldo Dharmawan <andyaldodharmawan@gmail.com>
2019-04-09 12:01:42 +0800

    Install vim plug

--- vim/install.sh
+++ vim/install.sh
@@ -1,1 +1,9 @@
+mkdir -p $HOME/.vim/autoload
+
+if [ -z $HOME/.vim/autoload/vim.plug ]; then
+  curl -fLo $HOME/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+else
+  echo "VIM Plug already installed. Moving on..."
+fi
+
 ln -s $HOME/.dotfiles/vim/.vimrc $HOME/.vimrc
