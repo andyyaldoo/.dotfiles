ed737993d9da349c0d1c037a419bc096459dd47e
Andy Aldo Dharmawan <andyaldodharmawan@gmail.com>
2019-06-08 19:57:46 +0800

    Update zsh/aliases/terraform.sh
    Always output plan and apply using outputted plan

--- zsh/aliases/terraform.sh
+++ zsh/aliases/terraform.sh
@@ -1,1 +1,4 @@
 alias t="terraform"
+alias tfplan='terraform plan -out=plan.tfplan -refresh=false'
+alias tffreshplan='terraform plan -out=plan.tfplan'
+alias tfapply='terraform apply plan.tfplan; rm plan.tfplan'
