54f3aec0fd1f51bdb9aae0bb2227880b9c49f0c7
Andy <adharmawan@moneylion.com>
2021-11-15 09:52:59 +0800

    Update install.sh

--- brew/install.sh
+++ brew/install.sh
@@ -6,6 +6,7 @@ if test ! $(which brew); then
   echo "\nLooks like you don't have homebrew. Installing it for you...\n"
   echo ""
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  eval "$(/opt/homebrew/bin/brew shellenv)"
 fi
 
 
