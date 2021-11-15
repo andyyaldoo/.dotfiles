5347dc86ddb776715b11c4a15fc1ced9fc80be78
Andy <adharmawan@moneylion.com>
2021-11-15 10:13:19 +0800

    Update zsh path

--- zsh/install.sh
+++ zsh/install.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 # Set zsh as the default shell
-if [ $SHELL == "/usr/local/bin/zsh" ]; then
+if [ $SHELL == "/bin/zsh" ]; then
     echo "\nzsh is already the default shell. Moving on...\n"
 else
     # check if /usr/local/bin/zsh is added to /etc/shells
-    if [ ! -z $(cat /etc/shells | grep /usr/local/bin/zsh) ]; then
-        echo "\n/usr/local/bin/zsh is added to /etc/shells. Moving on...\n"
+    if [ ! -z $(cat /etc/shells | grep /bin/zsh) ]; then
+        echo "\n/bin/zsh is added to /etc/shells. Moving on...\n"
     else
         # add /usr/local/bin/zsh to /etc/shells
-        sudo sh -c 'echo /usr/local/bin/zsh >> /etc/shells' 
+        sudo sh -c 'echo /bin/zsh >> /etc/shells' 
         if [ ! $? -eq 0 ]; then
             echo "Failed. trying other method..."
-            sudo bash -c "echo /usr/local/bin/zsh >> /etc/shells"
+            sudo bash -c "echo /bin/zsh >> /etc/shells"
             if [ !$? -eq 0 ]; then
                 echo "Failed. Aborting..."
                 exit 1
@@ -19,7 +19,7 @@ else
         fi
     fi
     # Set zsh as default shell
-    chsh -s /usr/local/bin/zsh
+    chsh -s /bin/zsh
     if [ ! $? -eq 0 ]; then
             echo Operation failed
             exit 1
