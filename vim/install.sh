1075d46c95699eb678ebca7d4aac10a4bff12820
Andy <andyyaldoo@gmail.com>
2021-11-17 12:09:51 +0800

    Fix file check

--- vim/install.sh
+++ vim/install.sh
@@ -1,12 +1,12 @@
 mkdir -p $HOME/.vim/autoload
 
-if [ -z $HOME/.vim/autoload/plug.vim ]; then
+if [ ! -f $HOME/.vim/autoload/plug.vim ]; then
   curl -fLo $HOME/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 else
   echo "vim plug already installed. Moving on..."
 fi
 
-if [ -z $HOME/.vim/colors/brogrammer.vim ]; then
+if [ ! -f $HOME/.vim/colors/brogrammer.vim ]; then
   mkdir -p $HOME/.vim/colors
   curl -fLo $HOME/.vim/colors/brogrammer.vim https://raw.githubusercontent.com/marciomazza/vim-brogrammer-theme/master/colors/brogrammer.vim
 else
