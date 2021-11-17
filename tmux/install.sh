e81c7a1feea043db32912a77c15174ad98ee2b5e
Andy <andyyaldoo@gmail.com>
2021-11-17 13:45:11 +0800

    Fix tmux.conf

--- tmux/install.sh
+++ tmux/install.sh
@@ -1,11 +1,11 @@
 # Install tmux
 
 
-if [ -z $HOME/.tmux/plugins/tpm ]; then
+if [ ! -d $HOME/.tmux/plugins/tpm ]; then
   mkdir -p $HOME/.tmux/plugins/tpm
   git clone https://github.com/tmux-plugins/tpm $HOME/.tmux/plugins/tpm
 else
   echo "tmux package manager (TPM) is already installed. Moving on..."
 fi
 
-ln -s $HOME/.dotfiles/tmux/.tmux.conf $HOME/.tmux.conf
+ln -fs $HOME/.dotfiles/tmux/.tmux.conf $HOME/.tmux.conf
